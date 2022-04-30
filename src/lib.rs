ba7b49796c588f6db63653f901681fe4b5b401c9
rpi400 <xue35@icloud.com>
2022-04-30 11:53:51 +0800

    Add 404 for root folder.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Router,http::{StatusCode}};
 use sync_wrapper::SyncWrapper;
 use shuttle_service::Error;
 
@@ -6,10 +6,16 @@ async fn hello_world() -> &'static str {
     "Hello shuttle."
 }
 
+async fn index() -> StatusCode {
+    StatusCode::NOT_FOUND
+}
+
 #[shuttle_service::main]
 async fn axum() -> Result<SyncWrapper<Router>, shuttle_service::Error> {
     let r = Router::new()
+            .route("/", get(index))
             .route("/hello", get(hello_world));
+
     let sync_wrapper = SyncWrapper::new(r);
     Ok(sync_wrapper)
 }
